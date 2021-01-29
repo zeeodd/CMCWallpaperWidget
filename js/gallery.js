eae96e856bebcf89ebee833645dbad6e3bf55dea
zeeodd <evergreenskier@gmail.com>
2021-01-29 13:36:19 -0800

    new images and fixed gallery selection code

--- js/gallery.js
+++ js/gallery.js
@@ -1,65 +1,143 @@
 // IMAGE 1
 document.getElementById('img1').addEventListener("click", function (e) {
-  ogImg = new fabric.Image(document.getElementById('img1'), {
-      top: 0,
-      left: 0,
-      selectable: false
-    });
+  var imgsrc = document.getElementById('img1').src;
 
-    clipImg = new fabric.Image(document.getElementById('img1'), {
-      top: 0,
-      left: 0,
-      centerX: "center",
-      centerY: "center",
-      selectable: false
-    });
+  ogImg = fabric.Image.fromURL(imgsrc, function (img) {
+    var scale = 1.0;
+    if (img.height > (canvasHeight-25) || img.width > (canvasWidth-25)) {
+      scale = (canvasHeight-25) / Math.max(img.height, img.width)
+    }
+    oImg = img.set({ left: 0,
+                         top: 0,
+                         angle: 0,
+                         width: img.width,
+                         height: img.height,
+                         selectable: false }).scale(scale);
+    canvasfabric.clear();
+    canvasfabric.add(oImg).renderAll();
+    canvasfabric.centerObject(oImg);
+  });
 
-    imageAdded = true;
-    canvasfabric.add(ogImg);
-    canvasfabric.centerObject(ogImg);
-    canvasfabric.renderAll();
+  clipImg = fabric.Image.fromURL(imgsrc, function (img) {
+    var scale = 1.0;
+    if (img.height > (canvasHeight-25) || img.width > (canvasWidth-25)) {
+      scale = (canvasHeight-25) / Math.max(img.height, img.width)
+    }
+    clipImg = img.set({ left: 0,
+                         top: 0,
+                         angle: 0,
+                         width: img.width,
+                         height: img.height,
+                         selectable: false }).scale(scale);
+  });
+
+  imageAdded = true;
 });
 
 // IMAGE 2
 document.getElementById('img2').addEventListener("click", function (e) {
-  ogImg = new fabric.Image(document.getElementById('img2'), {
-      top: 0,
-      left: 0,
-      selectable: false
-    });
+  var imgsrc = document.getElementById('img2').src;
 
-    clipImg = new fabric.Image(document.getElementById('img2'), {
-      top: 0,
-      left: 0,
-      centerX: "center",
-      centerY: "center",
-      selectable: false
-    });
+  ogImg = fabric.Image.fromURL(imgsrc, function (img) {
+    var scale = 1.0;
+    if (img.height > (canvasHeight-25) || img.width > (canvasWidth-25)) {
+      scale = (canvasHeight-25) / Math.max(img.height, img.width)
+    }
+    oImg = img.set({ left: 0,
+                         top: 0,
+                         angle: 0,
+                         width: img.width,
+                         height: img.height,
+                         selectable: false }).scale(scale);
+    canvasfabric.clear();
+    canvasfabric.add(oImg).renderAll();
+    canvasfabric.centerObject(oImg);
+  });
 
-    imageAdded = true;
-    canvasfabric.add(ogImg);
-    canvasfabric.centerObject(ogImg);
-    canvasfabric.renderAll();
+  clipImg = fabric.Image.fromURL(imgsrc, function (img) {
+    var scale = 1.0;
+    if (img.height > (canvasHeight-25) || img.width > (canvasWidth-25)) {
+      scale = (canvasHeight-25) / Math.max(img.height, img.width)
+    }
+    clipImg = img.set({ left: 0,
+                         top: 0,
+                         angle: 0,
+                         width: img.width,
+                         height: img.height,
+                         selectable: false }).scale(scale);
+  });
+
+  imageAdded = true;
 });
 
 // IMAGE 3
 document.getElementById('img3').addEventListener("click", function (e) {
-  ogImg = new fabric.Image(document.getElementById('img3'), {
-      top: 0,
-      left: 0,
-      selectable: false
-    });
+  var imgsrc = document.getElementById('img3').src;
 
-    clipImg = new fabric.Image(document.getElementById('img3'), {
-      top: 0,
-      left: 0,
-      centerX: "center",
-      centerY: "center",
-      selectable: false
-    });
+  ogImg = fabric.Image.fromURL(imgsrc, function (img) {
+    var scale = 1.0;
+    if (img.height > (canvasHeight-25) || img.width > (canvasWidth-25)) {
+      scale = (canvasHeight-25) / Math.max(img.height, img.width)
+    }
+    oImg = img.set({ left: 0,
+                         top: 0,
+                         angle: 0,
+                         width: img.width,
+                         height: img.height,
+                         selectable: false }).scale(scale);
+    canvasfabric.clear();
+    canvasfabric.add(oImg).renderAll();
+    canvasfabric.centerObject(oImg);
+  });
 
-    imageAdded = true;
-    canvasfabric.add(ogImg);
-    canvasfabric.centerObject(ogImg);
-    canvasfabric.renderAll();
+  clipImg = fabric.Image.fromURL(imgsrc, function (img) {
+    var scale = 1.0;
+    if (img.height > (canvasHeight-25) || img.width > (canvasWidth-25)) {
+      scale = (canvasHeight-25) / Math.max(img.height, img.width)
+    }
+    clipImg = img.set({ left: 0,
+                         top: 0,
+                         angle: 0,
+                         width: img.width,
+                         height: img.height,
+                         selectable: false }).scale(scale);
+  });
+
+  imageAdded = true;
+});
+
+// IMAGE 4
+document.getElementById('img4').addEventListener("click", function (e) {
+  var imgsrc = document.getElementById('img4').src;
+
+  ogImg = fabric.Image.fromURL(imgsrc, function (img) {
+    var scale = 1.0;
+    if (img.height > (canvasHeight-25) || img.width > (canvasWidth-25)) {
+      scale = (canvasHeight-25) / Math.max(img.height, img.width)
+    }
+    oImg = img.set({ left: 0,
+                         top: 0,
+                         angle: 0,
+                         width: img.width,
+                         height: img.height,
+                         selectable: false }).scale(scale);
+    canvasfabric.clear();
+    canvasfabric.add(oImg).renderAll();
+    canvasfabric.centerObject(oImg);
+  });
+
+  clipImg = fabric.Image.fromURL(imgsrc, function (img) {
+    var scale = 1.0;
+    if (img.height > (canvasHeight-25) || img.width > (canvasWidth-25)) {
+      scale = (canvasHeight-25) / Math.max(img.height, img.width)
+    }
+    clipImg = img.set({ left: 0,
+                         top: 0,
+                         angle: 0,
+                         width: img.width,
+                         height: img.height,
+                         selectable: false }).scale(scale);
+  });
+
+  imageAdded = true;
 });
--- js/papasheets.js
+++ js/papasheets.js
@@ -4,17 +4,40 @@ var imageGallery = document.getElementById("div3");
 var imageIDs = []
 
 Papa.parse(sheetURL, {
-    download: true,
-    complete: function(results) {
-      console.log(results.data);
-      for (var i = 0; i < results.data.length; i++) {
-        var newImage = document.createElement("img");
-        newImage.id = results.data[i][0];
-        imageIDs.push(results.data[i][0]);
-        newImage.classList.add("gallery-img");
-        newImage.src = results.data[i][1];
-        newImage.crossOrigin = "anonymous";
-        imageGallery.appendChild(newImage);
-      }
+  download: true,
+  complete: function(results) {
+    for (var i = 0; i < results.data.length; i++) {
+      var newImage = document.createElement("img");
+      newImage.id = results.data[i][0];
+      imageIDs.push(results.data[i][0]);
+      newImage.classList.add("gallery-img");
+      newImage.src = results.data[i][1];
+      newImage.crossOrigin = "anonymous";
+      imageGallery.appendChild(newImage);
+
+      newImageEl = document.getElementById(newImage.id);
+      console.log(newImageEl);
+
+      newImageEl.addEventListener("click", function (e) {
+        ogImg = new fabric.Image(newImageEl, {
+            top: 0,
+            left: 0,
+            selectable: false
+          });
+
+          clipImg = new fabric.Image(newImageEl, {
+            top: 0,
+            left: 0,
+            centerX: "center",
+            centerY: "center",
+            selectable: false
+          });
+
+          imageAdded = true;
+          canvasfabric.add(ogImg);
+          canvasfabric.centerObject(ogImg);
+          canvasfabric.renderAll();
+      });
     }
-   });
+  }
+});
